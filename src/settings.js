a2e2280cee8aed4fdc45b6b149135be7c21a2b84
Uzay Dalyan <uzay.dalyan@v-count.com>
2022-12-04 22:56:36 +0300

    token change

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,7 @@
 import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react"
 
 const appId = "651567b3ecc3473e806ec776ae53781a";
-const token = "007eJxTYLDtdSxa/cy2g/P6Tp/6gINRe36XX/7MKbEzZu/lPoMAtaUKDGamhqZm5knGqcnJxibmxqkWBmapyebmZomppsbmFoaJ9yd0JzcEMjJsM9zFyMgAgSA+C0NuYmYeAwMAepQgMw==";
+const token = "007eJxTYIh74OfMKu51Xjpn9g1F9RM+yd1LlOuF1pSVlDpePaQe8VmBwczU0NTMPMk4NTnZ2MTcONXCwCw12dzcLDHV1NjcwjDR9WNPckMgIwNfRzYTIwMEgvgsDLmJmXkMDACbzh0c";
 
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
