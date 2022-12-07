a6c10521b5f0602d072cc7677a67868f4ab23ee3
Uzay Dalyan <uzay.dalyan@v-count.com>
2022-12-07 23:49:56 +0300

    token change

--- src/settings.js
+++ src/settings.js
@@ -1,9 +1,9 @@
 import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react"
 
 const appId = "651567b3ecc3473e806ec776ae53781a";
-const token = "007eJxTYIh74OfMKu51Xjpn9g1F9RM+yd1LlOuF1pSVlDpePaQe8VmBwczU0NTMPMk4NTnZ2MTcONXCwCw12dzcLDHV1NjcwjDR9WNPckMgIwNfRzYTIwMEgvgsDLmJmXkMDACbzh0c";
+const token = "006651567b3ecc3473e806ec776ae53781aIADTCUk+rfPh/9LqIvv8AOtNVVD2tIe9PGFmZXDQMjLhO3Qs7/YAAAAAIgAV8hwADk2SYwQAAQCOTLZlAgCOTLZlAwCOTLZlBACOTLZl";
 
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
 export const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks();
-export const channelName = "main";
+export const channelName = "danistirchannel";
